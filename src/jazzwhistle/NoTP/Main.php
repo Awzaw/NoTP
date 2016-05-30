fb9f5631882735deaeeea1a4fb20b4edcacc2d9d
jazzwhistle <jdcraftmcpe@gmail.com>
2016-05-30 03:35:56 +0200

    Initial Commit Version 1.0.0

---
+++ src/jazzwhistle/NoTP/Main.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace jazzwhistle\NoTP;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->enabled = array();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onCommand(CommandSender $issuer, Command $cmd, $label, array $args) {
+        if ((strtolower($cmd->getName()) == "notp") && !(isset($args[0])) && ($issuer instanceof Player) && ($issuer->hasPermission("notp.toggle") || $issuer->hasPermission("notp.toggle.self"))) {
+            if (isset($this->enabled[strtolower($issuer->getName())])){
+                unset ($this->enabled[strtolower($issuer->getName())]);
+            }else{
+            $this->enabled[strtolower($issuer->getName())] = strtolower($issuer->getName());
+            }
+           
+            if (isset($this->enabled[strtolower($issuer->getName())])) {
+                $issuer->sendMessage("NoTP mode enabled!");
+            } else {
+                $issuer->sendMessage("NoTP mode disabled!");
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param PlayerCommandPreprocessEvent $event
+     *
+     * @priority MONITOR
+     */
+    public function onPlayerCommand(PlayerCommandPreprocessEvent $event) {
+
+        $message = $event->getMessage();
+        if (strtolower(substr($message, 0, 3) === "/tp")) { //Command
+            $command = substr($message, 1);
+            $args = explode(" ", $command);
+            if (!isset($args[1])){
+               return; 
+            }
+            $sender = $event->getPlayer();
+            
+            foreach ($this->enabled as $notpuser) {
+ 
+                if (strpos(strtolower($notpuser), strtolower($args[1])) !== false) {
+                    $sender->sendMessage(TextFormat::RED. "This Player Is Not Accepting TP");
+                    $event->setCancelled(true);
+                }
+                
+                if (isset($args[2]) && strpos(strtolower($notpuser), strtolower($args[2])) !== false) {
+                    $sender->sendMessage(TextFormat::RED. "This Player Is Not Accepting TP");
+                    $event->setCancelled(true);
+                }
+            }
+        }
+    }
+
+}
